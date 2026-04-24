0d4724ab3d036b5201f4760cf1b973e370ba00fa
Javier <Javier@gmail.com>
2026-04-24 13:53:02 +0200

    attr required added to registrationform

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,14 +18,19 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class)
+            ->add('email', EmailType::class,[
+                'required' => true
+            ])
+
             ->add('username', null, [
+                'required' => true,
                 'attr' => ['placeholder' => 'Nom d\'utilisateur']
             ])
             
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'required' => true,
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
