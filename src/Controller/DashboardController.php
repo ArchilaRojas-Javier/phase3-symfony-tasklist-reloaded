d4935ff7b21f93441b042b43aca373049fb67687
Javier <Javier@gmail.com>
2026-04-26 14:30:53 +0200

    crud of folder created, controller, forms and templates

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,9 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Form\Task1Type;
-use App\Entity\Task;
-use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 
 final class DashboardController extends AbstractController
